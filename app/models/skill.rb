94d6f4c26cd6edd5a5599bf663f47740c886d7c4
Drew Conly <atconly@gmail.com>
2015-07-07 15:52:43 -0600

    basic functionality with css

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,13 @@
 class Skill
-  
+
+  attr_reader :title,
+              :description,
+              :id
+
+  def initialize(data)
+    @title       = data["title"]
+    @description = data["description"]
+    @id          = data["id"]
+  end
+
 end
