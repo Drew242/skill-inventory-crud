52e3f9576b68686779e2b5853403992a6a9c48b6
Drew Conly <atconly@gmail.com>
2015-07-07 17:15:05 -0600

    fixed naming bugs

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -2,7 +2,8 @@ require 'models/skill_inventory'
 
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
-  set :method_override, true 
+  set :method_override, true
+  
   get '/' do
     erb :dashboard
   end
